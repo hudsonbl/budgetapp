afa9c66ccc3288b8cb6907a005036b9f9321fe35
blake <hudsonbl@oregonstate.edu>
2021-01-16 17:56:35 -0800

    First push to repo from vscode

---
+++ src/components/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginScreen = () => {
+
+
+    return (
+        <div className="container">
+            <h1>Login Screen</h1>
+        </div>
+    )
+}
+
+export default LoginScreen
